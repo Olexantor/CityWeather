8d2da02dc4299b70bb389cbf978ca983541c6cf0
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-08-08 14:09:10 +0500

    Storage Manager
    rename variables

--- CityWeather/StorageManager.swift
+++ CityWeather/StorageManager.swift
@@ -15,12 +15,12 @@ class StorageManager {
     private init() {}
     
     func save(city: String) {
-        var cities = fetchCities
-        cities.append(city)
-        userDefaults.set(cities, forKey: kCity)
+        var citiesForSave = cities
+        citiesForSave.append(city)
+        userDefaults.set(citiesForSave, forKey: kCity)
     }
     
-    var fetchCities: [String] {
+    var cities: [String] {
         if let cities = userDefaults.value(forKey: kCity) as? [String] {
             return cities
         }
@@ -28,8 +28,8 @@ class StorageManager {
     }
     
     func deleteCity(at index: Int) {
-        var cities = fetchCities
-        cities.remove(at: index)
-        userDefaults.set(cities, forKey: kCity)
+        var citiesForDelete = cities
+        citiesForDelete.remove(at: index)
+        userDefaults.set(citiesForDelete, forKey: kCity)
     }
 }
