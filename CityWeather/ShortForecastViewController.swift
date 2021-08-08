22843a9e41e94059ffc85489d2f646e7ae652ba0
Sergey Borovikov <sergey.borovikov@list.ru>
2021-08-08 21:40:36 +0500

    Added UITableView

--- CityWeather/ShortForecastViewController.swift
+++ CityWeather/ShortForecastViewController.swift
@@ -6,17 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
-class ShortForecastViewController: UITableViewController {
+class ShortForecastViewController: UIViewController {
     
-    var cities = StorageManager.shared.cities
+    private var cities = StorageManager.shared.cities
+    private var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
+        setupTableView()
     }
 
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "shortForecastCell")
+        view.addSubview(tableView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 
     private func setupNavigationBar() {
         title = "Short forecast"
@@ -52,35 +68,26 @@ class ShortForecastViewController: UITableViewController {
     @objc private func addNewCity() {
         
     }
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        cities.count
-//    }
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "shortForecastCell", for: indexPath)
-//
-//        let city = cities[indexPath.row]
-//        var content = cell.defaultContentConfiguration()
-//        content.text = city
-//
-//        return cell
-    
 }
 
-extension ShortForecastViewController {
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
+extension ShortForecastViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         cities.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "shortForecastCell", for: indexPath)
 
         let city = cities[indexPath.row]
-        var content = cell.defaultContentConfiguration()
-        content.text = city
+        cell.textLabel?.text = city
 
         return cell
     }
+}
+
+extension ShortForecastViewController: UITableViewDelegate {
+    
 }
 
