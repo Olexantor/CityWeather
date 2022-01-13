54bb1c56d38ca9a300bad497b5e9404d9730908f
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2022-01-13 20:39:33 +0500

    Constants file
    refresh key

--- CityWeather/Supporting files/Constants.swift
+++ CityWeather/Supporting files/Constants.swift
@@ -9,4 +9,4 @@ import Foundation
 
 let urlYandex = "https://api.weather.yandex.ru/v2/forecast?"
 let apiHeader = "X-Yandex-API-Key"
-let apiKey = "3ccd80e7-8122-4d79-b6a7-9eac5b3ea0df"
+let apiKey = "2d4483bd-5c0a-49ab-8a82-01d9859840dc"
